cbdec9128bbaf9c53fd2ccb00adbee1419da9085
Okan YÜKSEL <okannnyuksel@gmail.com>
2021-03-20 18:13:46 +0300

    primeng has been installed.

--- AdvancedMarketPlace/ClientApp/src/app/app.module.ts
+++ AdvancedMarketPlace/ClientApp/src/app/app.module.ts
@@ -10,6 +10,14 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { ListboxModule } from 'primeng/listbox';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,11 +30,17 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ListboxModule,
+    CheckboxModule,
+    ButtonModule,
+    DropdownModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'counter', component: CounterComponent },
+    { path: 'fetch-data', component: FetchDataComponent },
+], { relativeLinkResolution: 'legacy' })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AdvancedMarketPlace/ClientApp/src/app/home/home.component.ts
+++ AdvancedMarketPlace/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  cities: SelectItem[];
+  selectedCity : SelectItem;
+  
+  constructor() { }
+ 
+  ngOnInit() {
+    this.cities = [
+      { label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } },
+      { label: 'Rome', value: { id: 2, name: 'Rome', code: 'RM' } },
+      { label: 'London', value: { id: 3, name: 'London', code: 'LDN' } },
+      { label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } },
+      { label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } }
+    ];
+  }
+
 }
+
+
